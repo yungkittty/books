250ff1a4ff98a495eaf6e253d1e8c2b66652a061
yungkittty <quentin.woivre@gmail.com>
2021-08-15 19:06:17 +0200

    books-*, ...

--- packages/books-api/scripts/start.sh
+++ packages/books-api/scripts/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 handle_exit () {
-  echo "handle_exit ${?}";
+  docker compose down;
 }
 
 trap handle_exit EXIT;
+
+docker compose up --force-recreate --quiet-pull;
+
+node ./src/index.js;
